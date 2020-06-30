3a26d94e6ac083eabd89507274cff2602a0e4f89
Andrea Franco <andrea07021981@gmail.com>
2020-06-29 21:20:56 -0400

    first commit

---
+++ SaveTheFood/Controllers/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  SaveTheFood
+//
+//  Created by Andrea Franco on 2020-06-29.
+//  Copyright © 2020 Andrea Franco. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+}
--- SaveTheFood/LoginViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LoginViewController.swift
-//  SaveTheFood
-//
-//  Created by Andrea Franco on 2020-06-08.
-//  Copyright © 2020 Andrea Franco. All rights reserved.
-//
-
-import Foundation
