2e4fcf49d2dc52954ba9b0beb0a5ecb58f845499
Andrea Franco <andrea07021981@gmail.com>
2020-06-30 20:07:43 -0400

    Login

--- SaveTheFood/Controllers/SpashViewController.swift
+++ SaveTheFood/Controllers/SpashViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class SpashViewController: UIViewController {
-
+class SpashViewController: UIViewController, CAAnimationDelegate {
+    @IBOutlet var screenImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let pulseAnimation = CABasicAnimation(keyPath: "opacity")
+        pulseAnimation.duration = 1
+        pulseAnimation.fromValue = 0
+        pulseAnimation.toValue = 1
+        pulseAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        pulseAnimation.autoreverses = true
+        pulseAnimation.repeatCount = 3
+        pulseAnimation.delegate = self
+        screenImage.layer.add(pulseAnimation, forKey: nil)
     }
-
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        self.performSegue(withIdentifier: "SplashToLogin", sender: nil)
+    }
 
 }
 
