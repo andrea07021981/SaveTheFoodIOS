04b472096dcc7a69aca1db6c8c45e302c86047c3
Andrea Franco <andrea07021981@gmail.com>
2020-07-02 18:44:38 -0400

    Login

--- SaveTheFood/Controllers/LoginViewController.swift
+++ SaveTheFood/Controllers/LoginViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var pswTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,5 +26,22 @@ class LoginViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    @IBAction func signInPressed(_ sender: UIButton) {
+        if (emailTextField.text == "" ||
+            pswTextField.text == "") {
+                showAlert();
+        }
+    }
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Attention", message: "Email and password required", preferredStyle: .alert)
+        alert.view.backgroundColor = UIColor.white
+        alert.view.alpha = 0.6
+        alert.view.layer.cornerRadius = 15
+        self.present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            alert.dismiss(animated: true)
+        }
     }
 }
