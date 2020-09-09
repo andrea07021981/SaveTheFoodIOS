37f649457f4ab8c3b54cf4072f26c6024b51e687
Andrea Franco <andrea07021981@gmail.com>
2020-09-09 14:19:31 -0400

    Added example for autolayout and priorities

---
+++ SaveTheFood/Controllers/NewFoodViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewFoodController.swift
+//  SaveTheFood
+//
+//  Created by Andrea Franco on 2020-09-09.
+//  Copyright © 2020 Andrea Franco. All rights reserved.
+//
+
+import UIKit
+
+class NewFoodViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func tappedButton(_ sender: UIButton) {
+        sender.removeFromSuperview()
+    }
+}
